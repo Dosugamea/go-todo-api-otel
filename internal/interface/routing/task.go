8959c75df41008ed199e40b9e6cc6b85f85a3e8d
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-02-06 20:39:24 +0900

    feat(routing): add tasks routing initialization function

---
+++ internal/interface/routing/task.go
@@ -0,0 +1,14 @@
+package routing
+
+import (
+	"github.com/Dosugamea/go-todo-api-otel/internal/interface/handler"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterTaskRoutings(g *echo.Group, h handler.TaskHandler) {
+	g.POST("/tasks", h.Add())
+	g.GET("/tasks/:id", h.Get())
+	g.GET("/tasks", h.List())
+	g.PUT("/tasks/:id", h.Update())
+	g.DELETE("/tasks/:id", h.Delete())
+}
