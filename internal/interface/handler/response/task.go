43364a3105bcae97083ee2c497455f0e2e34dbdb
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-02-06 22:14:50 +0900

    feat(response): impl Bind methods

--- internal/interface/handler/response/task.go
+++ internal/interface/handler/response/task.go
@@ -1,5 +1,12 @@
 package response
 
+import (
+	"errors"
+	"time"
+
+	"github.com/Dosugamea/go-todo-api-otel/internal/model"
+)
+
 type TaskResponse struct {
 	ID          int    `json:"id"`
 	CreatedAt   string `json:"created_at"`
@@ -9,17 +16,71 @@ type TaskResponse struct {
 	IsCompleted bool   `json:"is_completed"`
 }
 
+func (t *TaskResponse) Bind(task *model.Task) error {
+	if task == nil {
+		return errors.New("task is nil")
+	}
+	t.ID = task.ID
+	t.CreatedAt = task.CreatedAt.Format(time.RFC3339)
+	t.UpdatedAt = task.UpdatedAt.Format(time.RFC3339)
+	t.Title = task.Title
+	t.Description = task.Description
+	t.IsCompleted = task.IsCompleted
+	return nil
+}
+
 type ListTaskResponse struct {
 	Tasks []TaskResponse `json:"tasks"`
 }
+
+func (r *ListTaskResponse) Bind(tasks []*model.Task) error {
+	newTasks := make([]TaskResponse, len(tasks))
+	for i, task := range tasks {
+		res := TaskResponse{}
+		if err := res.Bind(task); err != nil {
+			return err
+		}
+		newTasks[i] = res
+	}
+	r.Tasks = newTasks
+	return nil
+}
+
 type GetTaskResponse struct {
 	Task TaskResponse `json:"task"`
+}
+
+func (r *GetTaskResponse) Bind(task *model.Task) error {
+	res := TaskResponse{}
+	if err := res.Bind(task); err != nil {
+		return err
+	}
+	r.Task = res
+	return nil
 }
 
 type CreateTaskResponse struct {
 	Task TaskResponse `json:"task"`
 }
 
+func (r *CreateTaskResponse) Bind(task *model.Task) error {
+	res := TaskResponse{}
+	if err := res.Bind(task); err != nil {
+		return err
+	}
+	r.Task = res
+	return nil
+}
+
 type UpdateTaskResponse struct {
 	Task TaskResponse `json:"task"`
+}
+
+func (r *UpdateTaskResponse) Bind(task *model.Task) error {
+	res := TaskResponse{}
+	if err := res.Bind(task); err != nil {
+		return err
+	}
+	r.Task = res
+	return nil
 }
