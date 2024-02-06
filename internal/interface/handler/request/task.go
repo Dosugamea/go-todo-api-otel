6600873de7fedebbae8db2caa3bc98bf4514607d
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-02-06 22:01:45 +0900

    feat(request): add Bind methods

--- internal/interface/handler/request/task.go
+++ internal/interface/handler/request/task.go
@@ -1,12 +1,32 @@
 package request
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 type AddTaskRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
+}
+
+func (r *AddTaskRequest) Bind(c echo.Context) error {
+	if err := c.Bind(r); err != nil {
+		return err
+	}
+	// TODO: バリデーション
+	return nil
 }
 
 type UpdateTaskRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	IsCompleted bool   `json:"is_completed"`
+}
+
+func (r *UpdateTaskRequest) Bind(c echo.Context) error {
+	if err := c.Bind(r); err != nil {
+		return err
+	}
+	// TODO: バリデーション
+	return nil
 }
